a5bc75b1c456fdee034af81423aa91de14c3a7d0
Luciano Tiniaco <tiniacoluciano05@gmail.com>
2026-09-20 14:17:53 +0200

    fix: bootstrap pip en build_files.sh para Vercel

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,6 @@
 # Script de build que corre Vercel antes de desplegar: instala deps y
 # genera static/ -> staticfiles/ (whitenoise los sirve desde ahi).
 set -e
+python3.9 -m ensurepip --upgrade
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
